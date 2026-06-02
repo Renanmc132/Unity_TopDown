716cff0c33f48800dc79569aff26cfb827682c0f
Renanmc132 <renanmc100@gmail.com>
2026-06-02 13:08:36 -0300

    feat : Movimentacao do Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D _rb;
+    private float moveSpeed = 5f;
+    private Vector2 direction;
+
+
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
     }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+    }
+
 }
